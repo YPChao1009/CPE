2661b14a73e48811c8050ac7e289cb33add6e1d7
YPChao1009 <49547531+YPChao1009@users.noreply.github.com>
2019-04-12 23:53:34 +0800

    Add files via upload

---
+++ 2.Hashmat the brave warrior.cpp
@@ -0,0 +1,23 @@
+/*
+ *(CPE10407, UVA10055) Hashmat the brave warrior
+ * Author:Yun Pei Chao 
+ */
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	long long int a, b;
+	while (cin >> a >> b) {
+
+		cout << abs(b - a) << endl;
+
+	}
+
+
+
+
+	system("pause");
+	return 0;
+}
