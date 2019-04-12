6e453b5912af7e93420f049d3aabf43eb9618f5b
YPChao1009 <49547531+YPChao1009@users.noreply.github.com>
2019-04-13 00:02:56 +0800

    Add files via upload

---
+++ 3.Primary Arithmetic.cpp
@@ -0,0 +1,41 @@
+/*
+ *(CPE10404, UVA10035) Primary Arithmetic.cpp
+ * Author:Yun Pei Chao 
+ */
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int a, b;
+	
+	while (cin >> a >> b && (a != 0 && b != 0)) {
+		int count = 0;
+		int c=0;                                            //¶i¦ì
+		while ((a != 0 || b != 0)) {
+			if (a % 10 + b % 10 + c >= 10)
+			{
+				count++;
+				c = 1;
+			}
+			else {
+				c = 0;
+			}
+			a /= 10;
+			b /= 10;
+
+		}
+		if (count == 0)
+			cout << "No carry operation.\n";
+		else if (count == 1)
+			cout << count << " carry operation.\n";
+		else
+			cout<<count<<" carry operations.\n";
+
+	}
+
+	
+
+	system("pause");
+	return 0;
+}
