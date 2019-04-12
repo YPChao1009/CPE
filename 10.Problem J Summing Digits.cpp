c6bbe264149b4f99c01f366b45c10679c760d576
YPChao1009 <49547531+YPChao1009@users.noreply.github.com>
2019-04-13 00:41:01 +0800

    Add files via upload

---
+++ 10.Problem J Summing Digits.cpp
@@ -0,0 +1,38 @@
+/*
+ *(CPE10473, UVA11332) Problem J: Summing Digits.cpp
+ * Author:Yun Pei Chao 
+ */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int F(int a) {
+	int total=0;
+	while (a > 0) {                        //所有位數的數字相加
+		total += a % 10;
+		a /= 10;
+	}
+	return total;
+}
+
+int main() {
+	int n;
+	
+	while (cin >> n&& n!=0) {
+     
+		while (n > 9) {                   //變成十進位數的時候要做FUNCTION
+           int total=0;
+			total += F(n);
+			n = total;
+		}
+		cout << n << endl;
+	}
+	
+
+
+
+
+
+	system("pause");
+	return 0;
+}
