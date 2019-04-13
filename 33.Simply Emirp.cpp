e4e6a41d84c4e776f3b60bd90514bb0d292eb782
YPChao1009 <49547531+YPChao1009@users.noreply.github.com>
2019-04-13 09:03:50 +0800

    Add files via upload

---
+++ 33.Simply Emirp.cpp
@@ -0,0 +1,41 @@
+/*
+ *(CPE10428, UVA10235) Simply Emirp.cpp
+ * Author:Yun Pei Chao 
+ */
+#include<iostream>
+using namespace std;
+
+bool is_Prime(int a) {
+	for (int i = 2; i < a ; i++) {
+		if (a%i == 0)
+			return false;
+		
+	}
+}
+int main() {
+	
+	int n,rn;
+	while (cin >> n) {
+
+		int tmp = n;
+		int result = 0;
+		while (true) {                                  //********¼Æ¦r¤ÏÂà
+			int digit = tmp % 10;
+			result = result * 10 + digit;
+			tmp /= 10;
+			if (tmp == 0)
+				break;
+		}
+		rn = result;
+
+		if ((is_Prime(n)!=false) && (is_Prime(rn) != false)) {
+			cout << n << " is emirp.\n";
+		}
+		else if (is_Prime(n) == false)
+			cout << n << " is not prime.\n";
+		else if ((is_Prime(n) != false) && (is_Prime(rn) == false))
+			cout << n << " is prime.\n";
+	}
+	system("pause");
+	return 0;
+}
