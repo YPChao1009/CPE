76a8827015ed8482ee706bbb522d215c9a3ec9ca
YPChao1009 <49547531+YPChao1009@users.noreply.github.com>
2019-04-13 08:15:20 +0800

    Update and rename 16.What is the Probability!!(題目不太懂).cpp to What is the Probability!!(題目不太懂).cpp

---
+++ What is the Probability!!(題目不太懂).cpp
@@ -0,0 +1,27 @@
+/*
+ *(CPE10408, UVA10056) What is the Probability!!.cpp
+ * Author:Yun Pei Chao 
+ */
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+
+	int s;
+	cin >> s;
+	while (s--) {
+		long int n,i;
+		double p;
+		cin >> n >> p >> i;
+		if (p < 0.00001)                                      //太小不計
+			cout << "0.0000\n";
+		double q = 1 - p;
+		printf("%.4f\n",(pow(q,i-1)*p)/(1-pow(q,n)));         //(q的i-1次方*p)/(1-q的n次方)
+
+	}
+
+
+	system("pause");
+	return 0;
+}
