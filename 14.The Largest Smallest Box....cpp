50f75f7be3303e075134481dc83b93ca84e6d2ef
YPChao1009 <49547531+YPChao1009@users.noreply.github.com>
2019-04-13 10:49:37 +0800

    Add files via upload

---
+++ 14.The Largest Smallest Box....cpp
@@ -0,0 +1,35 @@
+/*
+ *(CPE10423, UVA10215) 14.The Largest Smallest Box....cpp
+ * Author:Yun Pei Chao 
+ */
+#include<iostream>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int Min(int a, int b) {
+	if (b > a)
+	{
+		return a;
+	}
+	else
+		return b;
+}
+
+int main() {
+
+
+	double L, W;
+		double max, min;
+	while (cin >>L >>W) {
+		
+		max = ((L + W) - sqrt(L*L - L * W + W * W)) / 6;
+		min = Min(L, W)*0.5;
+
+		printf("%.3f %.3f %.3f\n", max + (1E-6), 0.000, min + (1E-6));
+		
+	}
+	system("pause");
+	return 0;
+}
