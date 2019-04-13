9ebf9f9c43e8a86680e84a9e3be44d688671941d
YPChao1009 <49547531+YPChao1009@users.noreply.github.com>
2019-04-13 10:43:06 +0800

    Add files via upload

---
+++ 9.Fibonacci Freeze.cpp
@@ -0,0 +1,30 @@
+/*
+ *(CPE23561, UVA495) Fibonacci Freeze.cpp
+ * Author:Yun Pei Chao 
+ */
+#include<iostream>
+using namespace std;
+
+
+
+int main() {
+
+	int n;
+	int F[5000];
+	F[0] = 0, F[1] = 1;
+    int sum=0;
+	while (cin >> n) {
+		
+		for (int i = 2; i < 5000; i++) {
+			F[i] = F[i - 1] + F[i - 2];
+			if (n == i) {
+				sum = F[i];
+				break;
+				}
+		
+		}
+		cout << "The Fibonacci number for " << n << " is " << sum << endl;
+	}
+	system("pause");
+	return 0;
+}
