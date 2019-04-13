2324e95580cc2d6cb95b97144b613e3df7a16f1f
YPChao1009 <49547531+YPChao1009@users.noreply.github.com>
2019-04-13 08:22:16 +0800

    Add files via upload

---
+++ 19.Odd Sum.cpp
@@ -0,0 +1,33 @@
+/*
+ *(CPE10453, UVA10783) Odd Sum.cpp
+ * Author:Yun Pei Chao 
+ */
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+
+int main() {
+	int n;
+	cin >> n;
+	int count = 1;
+	while (n--) {
+		int a, b;
+		int sum[2] = { 0,0 };                         //¤À©_°¸case
+		int odd=0;
+		
+		cin >> a >> b;
+		for (int i = a; i <= b; i++) {
+			sum[i % 2] += i;
+			
+		}
+		odd = sum[1];
+		cout <<"Case "<<count<<": "<< odd << endl;
+		count++;
+		}
+	
+	system("pause");
+	return 0;
+}
