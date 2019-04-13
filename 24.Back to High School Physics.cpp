7f7e3647f5400c211f41099223f3da3782ad1d86
YPChao1009 <49547531+YPChao1009@users.noreply.github.com>
2019-04-13 08:38:51 +0800

    Add files via upload

---
+++ 24.Back to High School Physics.cpp
@@ -0,0 +1,22 @@
+/*
+ *(CPE10411, UVA10071) Back to High School Physics.cpp
+ * Author:Yun Pei Chao 
+ */
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int a, b;
+	while (cin >> a >> b) {
+		cout << 2 * a*b << endl;           //公式推導出來的
+	}
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
