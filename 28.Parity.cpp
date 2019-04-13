68781c03b979a4598ab2867efb64d45836a38cc2
YPChao1009 <49547531+YPChao1009@users.noreply.github.com>
2019-04-13 08:51:04 +0800

    Add files via upload

---
+++ 28.Parity.cpp
@@ -0,0 +1,25 @@
+/*
+ *(CPE10461, UVA10931) Parity.cpp
+ * Author:Yun Pei Chao 
+ */
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	while (cin >> n && n != 0) {
+		int s = 0;
+		while (n > 0) {
+			if (n % 2 != 0)
+				s++;
+			n /= 2;
+		}
+		cout << "The parity of " << n << " is " << s << " (mod 2).\n";
+	}
+
+
+
+
+	system("pause");
+	return 0;
+}
